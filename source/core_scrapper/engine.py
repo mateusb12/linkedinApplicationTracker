0241583240a19fe07524496a292451b979999a84
Mateus <matbessa12@gmail.com>
2024-11-11 22:03:57 -0300

    Implement LinkedIn Saved Jobs Scraping and Update .gitignore - Add JSON and IPython notebook patterns to .gitignore - Update CoreScrapper to navigate to LinkedIn saved jobs and extract job details - Introduce main function to execute the scraping process

--- source/core_scrapper/engine.py
+++ source/core_scrapper/engine.py
@@ -1,11 +1,58 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 from source.core_scrapper.selenium_utils import open_chrome, export_cookies
-
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.support.ui import WebDriverWait
 
 class CoreScrapper:
-    def __init__(self, driver: webdriver.Chrome = None, ):
+    def __init__(self, driver: webdriver.Chrome = None):
         self.driver = driver if driver else open_chrome()
+        self.driver.get("https://www.linkedin.com/my-items/saved-jobs/?cardType=APPLIED")
+        self.collect_data_from_current_page()
+        return
 
     def save_cookies(self):
         export_cookies(self.driver)
+
+    def collect_data_from_current_page(self):
+        print("Collecting data from current page...")
+        search_containers = WebDriverWait(self.driver, 10).until(
+            ec.presence_of_all_elements_located((By.CLASS_NAME, "reusable-search__result-container"))
+        )
+
+        for index, container in enumerate(search_containers):
+            try:
+                # Find the span with class `entity-result__title-text`
+                title_span = container.find_element(By.CLASS_NAME, "entity-result__title-text")
+                subtitle_div = container.find_element(By.XPATH,
+                                                      "/html/body/div[5]/div[3]/div/main/section/div/div[2]/div/ul/li[1]/div/div/div/div[2]/div[1]/div[2]")
+                secondary_subtitle_div = container.find_element(By.XPATH,
+                                                                "/html/body/div[5]/div[3]/div/main/section/div/div[2]/div/ul/li[1]/div/div/div/div[2]/div[1]/div[3]")
+                timestamp_div = container.find_element(By.CLASS_NAME, "reusable-search-simple-insight__text")
+
+                anchor_text = title_span.find_element(By.TAG_NAME, "a").text
+                subtitle_text = subtitle_div.text
+                secondary_subtitle_text = secondary_subtitle_div.text
+                timestamp_text = timestamp_div.text
+                print(f"Container {index + 1} Anchor Text: [{anchor_text}]")
+                print(f"Container {index + 1} Subtitle Text: [{subtitle_text}]")
+                print(f"Container {index + 1} Secondary Subtitle Text: [{secondary_subtitle_text}]")
+                print(f"Container {index + 1} Timestamp Text: [{timestamp_text}]")
+                print("\n")
+            except Exception as e:
+                print(f"Error in container {index + 1}: {e}")
+
+    # def __del__(self):
+    #     try:
+    #         self.save_cookies()
+    #     finally:
+    #         if self.driver:
+    #             self.driver.quit()
+
+def main():
+    core_scrapper = CoreScrapper()
+    print("Done!")
+
+if __name__ == '__main__':
+    main()
